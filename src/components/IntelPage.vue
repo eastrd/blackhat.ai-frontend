1cfd823232211c67c909a9e59dc6a47d4b6e5ada
eastrd <yjia285@student.monash.edu>
2018-05-03 22:47:50 +1000

    Adjusted Div ratio

--- src/components/IntelPage.vue
+++ src/components/IntelPage.vue
@@ -21,16 +21,15 @@ export default {
 <style>
 #wrapper {
   display: flex;
-  flex-wrap: wrap;
+  flex: 0 0 50%;
+  /* flex-wrap: wrap; */
 }
 
 #element {
   flex: 0 0 50%;
-  padding: 10px;
 }
 
 #wrapper #element {
   margin: 0;
-  box-sizing: border-box;
 }
 </style>
